8a748d94d239452a761d49d09810f15f359af267
marianaga2100 <82132215+marianaga2100@users.noreply.github.com>
2021-04-07 23:32:44 -0500

    Create ResInt.js

---
+++ disp/ResInt.js
@@ -0,0 +1,13 @@
+  
+/** Se utiliza para devolver un
+ * valor o un error, pues no
+ * usaremos excepciones. */
+export class ResInt {
+  /**
+   * @param {number} valor 
+   * @param {string} error  */
+  constructor(valor, error) {
+    this.valor = valor;
+    this.error = error;
+  }
+}
