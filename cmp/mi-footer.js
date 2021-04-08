3de5e48f5afe151a5d6e6578b7b81b0985812fc6
marianaga2100 <82132215+marianaga2100@users.noreply.github.com>
2021-04-07 23:37:50 -0500

    Create mi-footer.js

---
+++ cmp/mi-footer.js
@@ -0,0 +1,11 @@
+  
+class MiFooter
+  extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = /* html */
+      `&copy; 2021
+      Gilberto Pacheco Gallegos.`;
+  }
+}
+customElements.define(
+  "mi-footer", MiFooter);
