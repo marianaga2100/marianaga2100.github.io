2df42faa92205ed1434c22944b0e723d6e8b8340
marianaga2100 <82132215+marianaga2100@users.noreply.github.com>
2021-04-08 01:25:58 -0500

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -1,9 +1,10 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyBG8XkE_IU1bW_TvHir7EMZiggwl2GO_7o",
-    authDomain: "magaiot.firebaseapp.com",
-    projectId: "magaiot",
-    storageBucket: "magaiot.appspot.com",
-    messagingSenderId: "904936252398",
-    appId: "1:904936252398:web:ab83782c1c56769e4324ff"
+  apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
+  authDomain: "gilpgiotx.firebaseapp.com",
+  databaseURL: "https://gilpgiotx.firebaseio.com",
+  projectId: "gilpgiotx",
+  storageBucket: "gilpgiotx.appspot.com",
+  messagingSenderId: "362186415969",
+  appId: "1:362186415969:web:e732f8610cfdae9544ef20"
 });
