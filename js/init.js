aeaecc3a3f25e1e927dfee447769837acc09969a
marianaga2100 <82132215+marianaga2100@users.noreply.github.com>
2021-04-07 23:23:46 -0500

    Update and rename config.js to init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+// @ts-ignore
+firebase.firebaseConfig ({
+    apiKey: "AIzaSyBG8XkE_IU1bW_TvHir7EMZiggwl2GO_7o",
+    authDomain: "magaiot.firebaseapp.com",
+    projectId: "magaiot",
+    storageBucket: "magaiot.appspot.com",
+    messagingSenderId: "904936252398",
+    appId: "1:904936252398:web:ab83782c1c56769e4324ff"
+  });
