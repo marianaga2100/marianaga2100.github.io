0285a25d1b998fc2096658ea7feb3f446f7abf17
marianaga2100 <82132215+marianaga2100@users.noreply.github.com>
2021-04-08 00:36:20 -0500

    Delete js directory

--- js/init.js
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
- firebase.initializeApp({
-    apiKey: "AIzaSyBG8XkE_IU1bW_TvHir7EMZiggwl2GO_7o",
-    authDomain: "magaiot.firebaseapp.com",
-    projectId: "magaiot",
-    storageBucket: "magaiot.appspot.com",
-    messagingSenderId: "904936252398",
-    appId: "1:904936252398:web:ab83782c1c56769e4324ff"
-  });
