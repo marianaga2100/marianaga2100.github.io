7861d2cd7b670fdceabfc6295067c7678c756a11
marianaga2100 <82132215+marianaga2100@users.noreply.github.com>
2021-04-08 00:58:26 -0500

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -3,7 +3,7 @@ class MiFooter
   connectedCallback() {
     this.innerHTML = /* html */
       `&copy; 2021
-      Gilberto Pacheco Gallegos.`;
+     Marian Araceli García Alvarado, IC-52M.`;
   }
 }
 customElements.define(
